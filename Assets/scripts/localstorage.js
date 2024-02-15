b1e8e9ce5808217e4dfecc883534d67ee345f127
@Ceo-Sammarco <noah.kd@gmail.com>
2024-02-15 09:06:23 -0300

    @Ceo-Sammarco - Thu 15 Feb 2024

---
+++ Assets/scripts/localstorage.js
@@ -0,0 +1,118 @@
+
+
+"use strict";
+let 
+   _ = ( ...v ) => console.log( ...v ),
+   Users = JSON.parse( localStorage.getItem( "Users" ) ) || [],
+   cardTemplate = `
+   <card>
+      <header>
+         <user name>${ userNameInput.value }</user>
+      </header>
+      <bd>
+         <user email>
+            <b>email</b>
+            <t>${ userEmailInput.value }</t>
+         </user>
+         <user password>
+            <b>password</b>
+            <t>${ userPasswordInput.value }</t>
+         </user>
+      </bd>
+   </card>`
+;
+
+addEventListener(
+   "load",
+   () => {
+      let 
+         Users = []
+      ;
+
+      formUser.addEventListener(
+         "submit",
+         () => {
+            event.preventDefault();
+            validateForm();
+         }
+      );
+
+      let UsersList = JSON.parse( localStorage.getItem( "Users" ) );
+
+      UsersList.find( v => {
+         if( v.name == "Noely Oliveira Gangello" ) {
+            _( `filter: ${ v.id }` );
+            _( {v} );
+         }
+      } );
+
+   }
+);
+   
+   
+/* == [ CRUD ] 
+== == == == == == == == == */
+function validateForm() {
+   if(
+      userNameInput.value === "" ||
+      userPasswordInput.value === "" ||
+      userEmailInput.value === ""
+   ) {
+      _( "Campo vazio" );
+   } else {
+      acceptData();
+   }
+}
+
+function acceptData() {
+   Users.push( 
+      {
+         id: `User-${ Math.round( Math.random() * 99999999 ) }-${ Users.length + 1 }-${ Math.round( Math.random() * 99999999 ) }-${Users.length}`,
+         name: userNameInput.value,
+         password: userPasswordInput.value,
+         email: userEmailInput.value,
+      }
+   );
+
+   localStorage.setItem( 
+      "Users", 
+      JSON.stringify( Users ) 
+   );
+   createUserCard();
+}
+
+function createUserCard() {
+   usersSection.innerHTML = "";
+
+   Users.map( ( k, v ) => {
+      _( "Oi" );
+      return usersSection.innerHTML += `
+         <card>
+            <header>
+               <user name>${ k.name }</user>
+            </header>
+            <bd>
+               <user email>
+                  <b>email</b>
+                  <t>${ k.email }</t>
+               </user>
+               <user password>
+                  <b>password</b>
+                  <t>${ k.password }</t>
+               </user>
+            </bd>
+         </card>
+      `;
+      resetForm();
+   } );
+}
+
+function resetForm() {
+   [ userNameInput, 
+      userEmailInput,
+      userPasswordInput ].map(
+         v.value = ""
+      );
+}
+
+createUserCard();
